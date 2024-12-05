af393e4fb7963c6c3432aaa828427612f1bb8a96
yalsik <msdegoat@gmail.com>
2024-12-05 19:00:32 +0530

    initial commit

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,22 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const dbConnect = async () => {
+    try {
+        const client = await pool.connect();
+        console.log('Database connected');
+        console.log(`DB Host: ${client.host}`);
+        client.release();
+    } catch (error) {
+        console.error('Database connection failed', error.message);
+        process.exit(1);
+    }
+};
+
+export default dbConnect;
