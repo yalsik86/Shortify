918c7257f5096688074042d447cf804f28b430fa
yalsik <msdegoat@gmail.com>
2024-12-07 00:07:34 +0530

    resolve & analytics controller logic

--- backend/controllers/url.controllers.js
+++ backend/controllers/url.controllers.js
@@ -11,20 +11,50 @@ export const shortenURL = asyncHandler( async(req, res) => {
         throw new ApiError(400, "enter URL before proceeding")
     }
     
-    let url = await Url.findOne({ where: { longUrl } });
-    if(!url) {
-        const shortUrl = nanoid(8);
-        url = await Url.create({
-            shortUrl,
-            longUrl
-        })
-    } else {
+    const existingUrl = await Url.findOne({ where: { longUrl } });
+    if(existingUrl) {
         return res.status(409).json(
-            new ApiResponse(409, {shortUrl: url.shortUrl, longUrl: longUrl}, "URL already shortened")
-        );    
+            new ApiResponse(409, {shortUrl: existingUrl.shortUrl, longUrl: longUrl}, "URL recently shortened")
+        )
+    }
+
+    const shortUrl = nanoid(8);
+    const url = await Url.create({
+        shortUrl,
+        longUrl
+    })
+
+    return res.status(200).json(
+        new ApiResponse(200, {shortUrl: shortUrl, longUrl: longUrl}, "URL shortened successfully")
+    )
+} )
+
+export const resolveURL = asyncHandler(async (req, res) => {
+    const { shortUrl } = req.params;
+
+    const url = await Url.findOne({ where: { shortUrl } });
+    if (!url) {
+        throw new ApiError(404, "shortened URL not found");
+    }
+
+    url.clicks += 1;
+    await url.save();
+
+    return res.status(200).json(
+        new ApiResponse(200, { longUrl: url.longUrl }, "URL resolved successfully")
+    )
+});
+
+export const getAnalytics = asyncHandler(async (req, res) => {
+    const { shortUrl } = req.params;
+
+    const url = await Url.findOne({ where: { shortUrl } });
+
+    if (!url) {
+        throw new ApiError(404, "Short URL not found.");
     }
 
     return res.status(200).json(
-        new ApiResponse(200, {shortUrl: url.shortUrl, longUrl: longUrl}, "URL shortened successfully")
-    )
-} )
+        new ApiResponse(200, { shortUrl: url.shortUrl, longUrl: url.longUrl, clicks: url.clicks }, "URL analytics retrieved successfully.")
+    );
+});
--- backend/routes/url.routes.js
+++ backend/routes/url.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { shortenURL } from "../controllers/url.controllers.js";
+import { shortenURL, resolveURL, getAnalytics } from "../controllers/url.controllers.js";
 
 const urlRouter = Router();
 
 urlRouter.route("/shorten").post(shortenURL);
+urlRouter.route("/resolve/:shortUrl").get(resolveURL);
+urlRouter.route("/analytics/:shortUrl").get(getAnalytics);
 
 export default urlRouter;
