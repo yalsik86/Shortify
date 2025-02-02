7399d5661412cb9cc8a9cbd566fccd15fcd417cc
yalsik <msdegoat@gmail.com>
2025-02-02 21:59:00 +0530

    rate limitiner implement

--- backend/routes/url.routes.js
+++ backend/routes/url.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { shortenURL, resolveURL, getAnalytics } from "../controllers/url.controllers.js";
+import { rateLimiter } from "../middlewares/rateLimit.js";
 
 const urlRouter = Router();
 
-urlRouter.route("/shorten").post(shortenURL);
-urlRouter.route("/resolve/:shortUrl").get(resolveURL);
+urlRouter.route("/shorten").post(rateLimiter, shortenURL);
+urlRouter.route("/resolve/:shortUrl").get(rateLimiter, resolveURL);
 urlRouter.route("/analytics/:shortUrl").get(getAnalytics);
 
 export default urlRouter;
