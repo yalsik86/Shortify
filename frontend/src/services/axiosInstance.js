bb8473b184c92df302dd52fea0aea798aec31212
yalsik <msdegoat@gmail.com>
2025-09-05 19:40:20 +0530

    frontend base url update

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/v1/url", // Backend base URL
+    baseURL: "https://shortify-backend-donb.onrender.com/api/v1/url", // Backend base URL
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
